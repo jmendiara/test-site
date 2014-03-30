5588e6a4440601ba8e9573e1562bf74db0a52c44
Javier Mendiara Cañardo <javier@tid.es>
2014-03-30 11:57:34 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testSiteApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
